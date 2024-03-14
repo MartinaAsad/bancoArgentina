a751c6ce50f6db456132680ee63549e5d3339971
MartinaAsad <lasupermartu15@gmail.com>
2024-03-14 20:23:25 -0300

    termine la solapa Depositos

--- bancoArgentina/src/Interfaz/PaginaInicio.java
+++ bancoArgentina/src/Interfaz/PaginaInicio.java
@@ -7,8 +7,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -45,23 +43,9 @@ public class PaginaInicio extends JFrame {
     conexion con =  new conexion();
     Connection conexion = con.metodo_conexion();
     private JTextField depositos_nrocuenta;
-    private JTextField depositos_nombreusuario;
     private JTextField depositos_cantdepositar;
     private JTextField depositos_usuario;
     private JTextField depositos_total;
-
-public class PaginaInicio extends JFrame {
-
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JTextField usuario_logueado;
-	private JTextField nombre;
-	private JTextField genero;
-	private JTextField dni;
-	private JTextField direccion;
-	private JTextField telefono;
-	private JTextField fechaNacimiento;
-	private JTextField fecha_actual;
 	/*Connection conn;
 	ResultSet rs;
 	PreparedStatement pst;*/
@@ -223,11 +207,6 @@ public class PaginaInicio extends JFrame {
         opciones.addTab("Depositos", null, depositos, null);
         depositos.setLayout(null);
 
-        JLabel texto_nombre_deposito = new JLabel("Nombre del usuario a depositar:");
-        texto_nombre_deposito.setFont(new Font("Arial", Font.PLAIN, 16));
-        texto_nombre_deposito.setBounds(41, 207, 228, 19);
-        depositos.add(texto_nombre_deposito);
-
         JLabel texto_depositos_nrocuenta = new JLabel("Numero de cuenta:");
         texto_depositos_nrocuenta.setFont(new Font("Arial", Font.PLAIN, 16));
         texto_depositos_nrocuenta.setBounds(41, 101, 153, 14);
@@ -244,18 +223,36 @@ public class PaginaInicio extends JFrame {
         depositos.add(depositos_nrocuenta);
         depositos_nrocuenta.setColumns(10);
 
-        depositos_nombreusuario = new JTextField();
-        depositos_nombreusuario.setBounds(314, 164, 122, 20);
-        depositos.add(depositos_nombreusuario);
-        depositos_nombreusuario.setColumns(10);
-
         depositos_cantdepositar = new JTextField();
-        depositos_cantdepositar.setBounds(314, 195, 122, 20);
+        depositos_cantdepositar.setBounds(314, 174, 122, 20);
         depositos.add(depositos_cantdepositar);
         depositos_cantdepositar.setColumns(10);
 
-        JButton depositos_boton_depositar = new JButton("Depositar");
-        depositos_boton_depositar.setBounds(451, 267, 89, 23);
+        JButton depositos_boton_depositar = new JButton("Depositar");/*chequear*/
+        depositos_boton_depositar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double totalDisponible=Double.parseDouble(depositos_total.getText());
+                    double valorADepo=Double.parseDouble(depositos_cantdepositar.getText());
+                    double balanceActualizado=totalDisponible+valorADepo;
+                    String balance=Double.toString(balanceActualizado);
+                    String usuario=depositos_usuario.getText();
+
+                    PreparedStatement stmt = null;
+                    ResultSet resultSet = null;
+                    String sql="UPDATE balances SET totalDisponible='"+balanceActualizado+"'where nombreCliente='"+usuario+"'";
+                    stmt=conexion.prepareStatement(sql);
+                    stmt.execute();
+                    JOptionPane.showMessageDialog(null, "deposito realizado");
+                    depositos_total.setText(balance);
+                    camposVaciosDeposito();
+
+                }catch(Exception e2) {
+                    JOptionPane.showMessageDialog(null, e2);
+                }
+            }
+        });
+        depositos_boton_depositar.setBounds(447, 233, 89, 23);
         depositos.add(depositos_boton_depositar);
 
         JLabel texto_depositos_usuario = new JLabel("Usuario:");
@@ -287,7 +284,7 @@ public class PaginaInicio extends JFrame {
                 String sql="SELECT * FROM balances WHERE nombreCliente = ?";
                 try {
                     stmt=conexion.prepareStatement(sql);
-                    stmt.setString(1, depositos_nombreusuario.getText());
+                    stmt.setString(1, depositos_usuario.getText());
                     resultSet=stmt.executeQuery();
                     if(resultSet.next()) {
                         /*obtengo los valores y los seteo*/
@@ -364,134 +361,6 @@ public class PaginaInicio extends JFrame {
                     stmt.setString(1, usuario_logueado.getText());
                     resultSet=stmt.executeQuery();
 					/*nombre.setEditable(true);
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					PaginaInicio frame = new PaginaInicio();
-					frame.setVisible(true);
-					
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	
-	/*metodo para obtener la fecha actual*/
-	//public void obtener_fecha_actual() {
-		
-	//}
-	
-	public PaginaInicio() {
-		conexion c= new conexion();
-		c.metodo_conexion();
-		setTitle("Home Banking Banco Argentina");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 628, 477);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel texto_usuario = new JLabel("Usuario:");
-		texto_usuario.setFont(new Font("Arial", Font.PLAIN, 16));
-		texto_usuario.setBounds(388, 11, 77, 14);
-		contentPane.add(texto_usuario);
-		
-		usuario_logueado = new JTextField();
-		usuario_logueado.setFont(new Font("Arial", Font.PLAIN, 11));
-		usuario_logueado.setBounds(502, 11, 86, 20);
-		contentPane.add(usuario_logueado);
-		usuario_logueado.setColumns(10);
-		
-		JTabbedPane opciones = new JTabbedPane(JTabbedPane.TOP);
-		opciones.setBounds(10, 79, 592, 348);
-		contentPane.add(opciones);
-		
-		JPanel perfil = new JPanel();
-		opciones.addTab("Perfil", null, perfil, null);
-		perfil.setLayout(null);
-		
-		JLabel texto_nombre = new JLabel("Nombre:");
-		texto_nombre.setFont(new Font("Arial", Font.PLAIN, 16));
-		texto_nombre.setBounds(10, 11, 73, 14);
-		perfil.add(texto_nombre);
-		
-		JLabel texto_genero = new JLabel("Genero:");
-		texto_genero.setFont(new Font("Arial", Font.PLAIN, 16));
-		texto_genero.setBounds(10, 50, 73, 14);
-		perfil.add(texto_genero);
-		
-		JLabel texto_dni = new JLabel("DNI:");
-		texto_dni.setFont(new Font("Arial", Font.PLAIN, 16));
-		texto_dni.setBounds(10, 93, 46, 14);
-		perfil.add(texto_dni);
-		
-		JLabel texto_direccion = new JLabel("Direccion:");
-		texto_direccion.setFont(new Font("Arial", Font.PLAIN, 16));
-		texto_direccion.setBounds(10, 134, 73, 14);
-		perfil.add(texto_direccion);
-		
-		JLabel texto_telefono = new JLabel("Telefono:");
-		texto_telefono.setFont(new Font("Arial", Font.PLAIN, 16));
-		texto_telefono.setBounds(10, 176, 73, 14);
-		perfil.add(texto_telefono);
-		
-		JLabel texto_fecha_n = new JLabel("Fecha de nacimiento:");
-		texto_fecha_n.setFont(new Font("Arial", Font.PLAIN, 16));
-		texto_fecha_n.setBounds(10, 219, 166, 14);
-		perfil.add(texto_fecha_n);
-		
-		nombre = new JTextField();
-		nombre.setEditable(false);
-		nombre.setBounds(219, 10, 159, 20);
-		perfil.add(nombre);
-		nombre.setColumns(10);
-		
-		genero = new JTextField();
-		genero.setEditable(false);
-		genero.setBounds(219, 49, 159, 20);
-		perfil.add(genero);
-		genero.setColumns(10);
-		
-		dni = new JTextField();
-		dni.setEditable(false);
-		dni.setBounds(219, 92, 159, 20);
-		perfil.add(dni);
-		dni.setColumns(10);
-		
-		direccion = new JTextField();
-		direccion.setEditable(false);
-		direccion.setBounds(219, 133, 159, 20);
-		perfil.add(direccion);
-		direccion.setColumns(10);
-		
-		telefono = new JTextField();
-		telefono.setEditable(false);
-		telefono.setBounds(219, 175, 159, 20);
-		perfil.add(telefono);
-		telefono.setColumns(10);
-		
-		fechaNacimiento = new JTextField();
-		fechaNacimiento.setEditable(false);
-		fechaNacimiento.setBounds(219, 218, 159, 20);
-		perfil.add(fechaNacimiento);
-		fechaNacimiento.setColumns(10);
-		
-		JButton boton_editar = new JButton("Editar campos");
-		boton_editar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				/*habilitar los campos para editarlos*/
-				nombre.setEditable(true);
 				genero.setEditable(true);
 				dni.setEditable(true);
 				direccion.setEditable(true);
@@ -548,65 +417,13 @@ public class PaginaInicio extends JFrame {
         contentPane.add(boton_ver_nombre_usuario);
 
 
+
     }
-}
-				fechaNacimiento.setEditable(true);
-				
-			}
-		});
-		boton_editar.setBounds(255, 274, 123, 23);
-		perfil.add(boton_editar);
-		
-		JButton boton_guardar = new JButton("Guardar cambios");
-		boton_guardar.setBounds(438, 274, 132, 23);
-		perfil.add(boton_guardar);
-		
-		JPanel depositos = new JPanel();
-		opciones.addTab("Depositos", null, depositos, null);
-		depositos.setLayout(null);
-		
-		JPanel transferencias = new JPanel();
-		opciones.addTab("Tranferencias", null, transferencias, null);
-		transferencias.setLayout(null);
-		
-		JPanel retiros = new JPanel();
-		opciones.addTab("Retiros", null, retiros, null);
-		retiros.setLayout(null);
-		
-		JPanel listaClientes = new JPanel();
-		opciones.addTab("Lista de clientes", null, listaClientes, null);
-		listaClientes.setLayout(null);
-		
-		JPanel historial = new JPanel();
-		opciones.addTab("Historial", null, historial, null);
-		historial.setLayout(null);
-		
-		JPanel balance = new JPanel();
-		opciones.addTab("Balance", null, balance, null);
-		balance.setLayout(null);
-		
-		JPanel cambiarPin = new JPanel();
-		opciones.addTab("Cambiar pin", null, cambiarPin, null);
-		cambiarPin.setLayout(null);
-		
-		JLabel texto_fecha = new JLabel("Fecha de hoy:");
-		texto_fecha.setFont(new Font("Arial", Font.PLAIN, 16));
-		texto_fecha.setBounds(388, 36, 110, 26);
-		contentPane.add(texto_fecha);
-		
-		fecha_actual = new JTextField();
-		fecha_actual.setBounds(502, 41, 86, 20);
-		contentPane.add(fecha_actual);
-		fecha_actual.setColumns(10);
-		/*setear la fecha actua*/
-		Calendar calendar=new GregorianCalendar();
-		int mes=calendar.get(Calendar.MONTH);//se obtiene el mes
-		int anio=calendar.get(Calendar.YEAR);//se obtiene el anio
-		int dia=calendar.get(Calendar.DAY_OF_MONTH);//se obtiene el dia
-		
-		//setear lo anterior para que se pueda visualizar
-		fecha_actual.setText(dia+"/"+(mes+1)+"/"+anio);
-		
-		
-	}
+    public void camposVaciosDeposito() {
+        depositos_cantdepositar.setText("");
+        depositos_nrocuenta.setText("");
+        depositos_total.setText("");
+        depositos_usuario.setText("");
+
+    }
 }
